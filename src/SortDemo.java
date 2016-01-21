c2c6b5df41b28ce9b9a96e41dda28bfc663ecb81
salilkansal <salilkansal.92@gmail.com>
2016-01-20 18:31:07 -0600

    Add the Heap Sort code

--- src/SortDemo.java
+++ src/SortDemo.java
@@ -89,7 +89,7 @@ public class SortDemo {
     public static void main(String args[]) {
         ArrayList<Integer> arrayList = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            arrayList.add(-2*i);
+            arrayList.add(-i);
         }
         System.out.println(arrayList);
 
